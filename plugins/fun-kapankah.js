28c2ed660653d270c23aa7a8d96903edcc56f025
chibbiyo <103373807+chibbiyo@users.noreply.github.com>
2022-06-03 22:07:25 +0100

    Update fun-kapankah.js

--- plugins/fun-kapankah.js
+++ plugins/fun-kapankah.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn }) => conn.reply(m.chat, `
-*Pertanyaan:* ${m.text}
-*Jawaban:* ${(10).getRandom()} ${['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'].getRandom()} lagi ...
+*Question:* ${m.text}
+*Answer:* ${(10).getRandom()} ${['second', 'minute', 'hour', 'day', 'Sunday', 'moon', 'year', 'decade', 'century'].getRandom()} lagi ...
   `.trim(), m, m.mentionedJid ? {
     mentions: m.mentionedJid
 } : {})
 
-handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <text>?')
-handler.tags = ['kerang', 'fun']
+handler.help = ['kah'].map(v => 'when' + v + ' <text>?')
+handler.tags = ['shellfish', 'fun']
 handler.customPrefix = /(\?$)/
-handler.command = /^kapan(kah)?$/i
+handler.command = /^when(kah)?$/i
 
 export default handler
