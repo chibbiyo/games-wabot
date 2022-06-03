fadc281bcc0249beddbe4f6ad33688e75d785776
chibbiyo <103373807+chibbiyo@users.noreply.github.com>
2022-06-03 13:25:58 +0100

    Update fun-apakah.js

--- plugins/fun-apakah.js
+++ plugins/fun-apakah.js
@@ -1,12 +1,12 @@
 let handler = async (m) => m.reply(`
 *Pertanyaan:* ${m.text}
-*Jawaban:* ${['Ya', 'Mungkin iya', 'Mungkin', 'Mungkin tidak', 'Tidak', 'Tidak mungkin'].getRandom()}
+*Jawaban:* ${['Ya', 'Maybe yes', 'Maybe', 'Probably not', 'Do not', 'Impossible'].getRandom()}
   `.trim(), null, m.mentionedJid ? {
   mentions: m.mentionedJid
 } : {})
 
-handler.help = ['apakah <teks>?']
-handler.tags = ['kerang', 'fun']
+handler.help = ['whether <teks>?']
+handler.tags = ['shellfish', 'fun']
 handler.customPrefix = /(\?$)/
 handler.command = /^apakah$/i
 
