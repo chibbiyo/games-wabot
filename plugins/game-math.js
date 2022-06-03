8cb842ced47107ecb813c8d7f64eac84a97ec7f2
chibbiyo <103373807+chibbiyo@users.noreply.github.com>
2022-06-03 22:53:40 +0100

    Update game-math.js

--- plugins/game-math.js
+++ plugins/game-math.js
@@ -3,21 +3,21 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     const buttons = Object.keys(modes).map(v => [v, `${usedPrefix}${command} ${v}`])
     if (args.length < 1) return conn.sendButton(m.chat, `
   Mode: ${Object.keys(modes).join(' | ')}
-  Contoh penggunaan: ${usedPrefix}math medium
+  Usage example: ${usedPrefix}math medium
   `.trim(), author, null, buttons, m)
     let mode = args[0].toLowerCase()
     if (!(mode in modes)) return conn.sendButton(m.chat, `
   Mode: ${Object.keys(modes).join(' | ')}
-  Contoh penggunaan: ${usedPrefix}math medium
+  Usage example: ${usedPrefix}math medium
     `.trim(), author, null, buttons, m)
     let id = m.chat
-    if (id in conn.math) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.math[id][0])
+    if (id in conn.math) return conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.math[id][0])
     let math = genMath(mode)
     conn.math[id] = [
-        await conn.reply(m.chat, `Berapa hasil dari *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nBonus Jawaban Benar: ${math.bonus} XP`, m),
+        await conn.reply(m.chat, `What is the result of *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nBonus Correct Answer: ${math.bonus} XP`, m),
         math, 4,
         setTimeout(() => {
-            if (conn.math[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah ${math.result}`, author, null, [['again', `${usedPrefix}${command} ${math.mode}`], ...buttons], conn.math[id][0])
+            if (conn.math[id]) conn.sendButton(m.chat, `Time out!\nThe answer is ${math.result}`, author, null, [['again', `${usedPrefix}${command} ${math.mode}`], ...buttons], conn.math[id][0])
             delete conn.math[id]
         }, math.time)
     ]
