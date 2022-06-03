9bf4417ff97cf55ef69b57b876c7075453def158
chibbiyo <103373807+chibbiyo@users.noreply.github.com>
2022-06-03 22:38:49 +0100

    Update game-caklontong_ans.js

--- plugins/game-caklontong_ans.js
+++ plugins/game-caklontong_ans.js
@@ -4,22 +4,22 @@ import db from '../lib/database.js'
 const threshold = 0.72
 export async function before(m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Ketik.*calo/i.test(m.quoted.text) || /.*(calo|bantuan)/i.test(m.text))
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Type.*ticket scalper/i.test(m.quoted.text) || /.*(ticket scalper|help)/i.test(m.text))
         return !0
     this.caklontong = this.caklontong ? this.caklontong : {}
     if (!(id in this.caklontong))
-        return m.reply('Soal itu telah berakhir')
+        return m.reply('The matter has end')
     if (m.quoted.id == this.caklontong[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.caklontong[id][1]))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             db.data.users[m.sender].exp += this.caklontong[id][2]
-            await this.sendButton(m.chat, `*Benar!* +${this.caklontong[id][2]} XP\n${json.deskripsi}`, author, null, [['Cak Lontong', '.caklontong']], m)
+            await this.sendButton(m.chat, `*True!* +${this.caklontong[id][2]} XP\n${json.deskripsi}`, author, null, [['Cak Lontong', '.caklontong']], m)
             clearTimeout(this.caklontong[id][3])
             delete this.caklontong[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
-            m.reply(`*Dikit Lagi!*`)
+            m.reply(`*Again!*`)
         else
-            m.reply(`*Salah!*`)
+            m.reply(`*Wrong!*`)
     }
     return !0
 }
