dfecbbdb6d8dedfba502606f3844a694ed739b88
chibbiyo <103373807+chibbiyo@users.noreply.github.com>
2022-06-03 13:12:49 +0100

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler =  m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
+╭─「 Donasi • ??? 」
+│ • Indosat Ooredoo [...]
+│ • Telkomsel [...]
 ╰────
 
-╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+╭─「 Donasi • ??? 」
+│ • https://???/????
+│ • Gopay [...]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
